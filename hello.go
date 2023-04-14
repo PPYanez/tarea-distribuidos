c620a6d22c981bda186b25eed8f3f3c008966a9f
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-13 20:57:29 -0400

    método get y función para generar pnrs

--- hello.go
+++ hello.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
+	"strings"
 	"time"
 
 	"net/http"
@@ -14,11 +16,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var client *mongo.Client
-
+// Estructuras
 type reserva struct {
 	Vuelos    []vuelo    `json:"vuelos,omitempty" bson:"vuelos,omitempty"`
 	Pasajeros []pasajero `json:"pasajeros,omitempty" bson:"pasajeros,omitempty"`
+	PNR       string     `json:"pnr,omitempty" bson:"pnr,omitempty"`
 }
 
 type pasajero struct {
@@ -70,8 +72,14 @@ type avion struct {
 	StockDePasajeros int    `json:"stock_de_pasajeros,omitempty" bson:"stock_de_pasajeros,omitempty"`
 }
 
+type PNR struct {
+	Value string `json:"value,omitempty" bson:"value,omitempty"`
+}
+
+var client *mongo.Client
+
 func main() {
-	/* Connect to mongodb */
+	// Conectarse a MongoDB
 	Mongo_URL := "mongodb://localhost:27017"
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(Mongo_URL).SetServerAPIOptions(serverAPI)
@@ -95,17 +103,21 @@ func main() {
 	}
 	fmt.Println("Successfully connected to MongoDB!")
 
-	/* Set up gin server */
+	// Levantar servidor Gin
 	router := gin.Default()
 
 	router.POST("/api/reserva", postReserva)
+	router.GET("/api/reserva", getReserva)
 
 	router.Run("localhost:6666")
 }
 
 func postReserva(c *gin.Context) {
-	var collection = client.Database("distribuidos").Collection("Reservas")
+	var reservaCollection = client.Database("distribuidos").Collection("Reservas")
+	var pnrCollection = client.Database("distribuidos").Collection("PNR")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	// Bindear JSON a estructura Reserva
 	nueva_reserva := new(reserva)
 
 	defer cancel()
@@ -116,36 +128,130 @@ func postReserva(c *gin.Context) {
 		return
 	}
 
+	// Guardar reserva en la base de datos
+	pnrReserva := GenPNR()
+
 	payload := reserva{
 		Vuelos:    nueva_reserva.Vuelos,
 		Pasajeros: nueva_reserva.Pasajeros,
+		PNR:       pnrReserva.Value,
 	}
 
-	result, err := collection.InsertOne(ctx, payload)
-
+	_, err := pnrCollection.InsertOne(ctx, pnrReserva)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"message": "Posted successfully", "Data": map[string]interface{}{"data": result}})
-}
-
-func getVuelos(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) *mongo.Cursor {
-	fmt.Println("Entered getvuelos")
-	collection := client.Database("distribuidos").Collection("vuelos")
-	fmt.Println("got colletion " + collection.Name() + " " + collection.Database().Name())
-
-	defer cancel()
-	fmt.Println("after cancel")
-
-	cursor, err := collection.Find(context.TODO(), bson.D{})
-	fmt.Println("got cursor")
-
+	_, err = reservaCollection.InsertOne(ctx, payload)
 	if err != nil {
-		panic(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
 	}
 
-	fmt.Println("return")
-	return cursor
+	c.JSON(http.StatusCreated, gin.H{"PNR": pnrReserva.Value})
+
+}
+
+func getReserva(c *gin.Context) {
+	PNR := c.Query("pnr")
+	apellido := c.Query("apellido")
+
+	if PNR == "" || apellido == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "No se ha ingresado un PNR o apellido"})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var collection = client.Database("distribuidos").Collection("Reservas")
+
+	var result reserva
+
+	defer cancel()
+
+	err := collection.FindOne(ctx, bson.M{"pnr": PNR, "pasajeros.apellido": apellido}).Decode(&result)
+	res := map[string]interface{}{"vuelos": result.Vuelos, "pasajeros": result.Pasajeros}
+
+	if err != nil {
+		c.JSON(
+			http.StatusNotFound,
+			gin.H{
+				"message": "No existe una reserva con los datos ingresados, por favor verifique que están bien escritos.",
+			},
+		)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"vuelos": res["vuelos"], "pasajeros": res["pasajeros"]})
+}
+
+func GenPNR() PNR {
+	var letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	var numbers = []rune("0123456789")
+	var allCharacters = append(letters, numbers...)
+	existingPNR := getExistingPNR()
+
+	var generatedPNR []rune
+
+	for true {
+		pnrIsNew := true
+
+		// generate pnr
+		generatedPNR = make([]rune, 5)
+
+		for i := range generatedPNR {
+			generatedPNR[i] = allCharacters[rand.Intn(len(allCharacters))]
+		}
+
+		if !strings.ContainsAny(string(generatedPNR), "0123456789") {
+			generatedPNR = append(generatedPNR, numbers[rand.Intn(len(numbers))])
+		} else if !strings.ContainsAny(string(generatedPNR), "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+			generatedPNR = append(generatedPNR, letters[rand.Intn(len(letters))])
+		} else {
+			generatedPNR = append(generatedPNR, allCharacters[rand.Intn(len(allCharacters))])
+		}
+
+		// check if pnr is new
+		for _, pnrInDB := range existingPNR {
+			if pnrInDB.Value == string(generatedPNR) {
+				pnrIsNew = false
+				break
+			}
+		}
+
+		if pnrIsNew {
+			break
+		}
+	}
+
+	return PNR{Value: string(generatedPNR)}
+}
+
+func getExistingPNR() []PNR {
+	var pnrCollection = client.Database("distribuidos").Collection("PNR")
+	findOptions := options.Find()
+	var results []PNR
+
+	cur, err := pnrCollection.Find(context.TODO(), bson.D{{}}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for cur.Next(context.TODO()) {
+		var elem PNR
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cur.Close(context.TODO())
+
+	return results
 }
